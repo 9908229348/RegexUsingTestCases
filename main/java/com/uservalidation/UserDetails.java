d4fecd7c7534636bf2afe7aa686d52919c16ca5a
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-26 11:48:40 +0530

    Added: Program for email validation

--- main/java/com/uservalidation/UserDetails.java
+++ main/java/com/uservalidation/UserDetails.java
@@ -3,10 +3,16 @@ package com.uservalidation;
 import java.util.regex.Pattern;
 
 public class UserDetails {
-    private final static String FirstName = "([A-Z][a-zA-Z]{2,})$";
+    private final static String FIRST_NAME = "([A-Z][a-zA-Z]{2,})$";
+    private final static String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 
     public boolean validateName(String name){
-        Pattern pattern = Pattern.compile(FirstName);
+        Pattern pattern = Pattern.compile(FIRST_NAME);
         return pattern.matcher(name).matches();
+    }
+
+    public boolean validateEmail(String email){
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
     }
 }
