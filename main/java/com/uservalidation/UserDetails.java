3ed9e0b7b85aaf1de47cff8353fbabe0558be26c
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-26 12:04:06 +0530

    Added: Program for MobileNumber Validation

--- main/java/com/uservalidation/UserDetails.java
+++ main/java/com/uservalidation/UserDetails.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserDetails {
     private final static String FIRST_NAME = "([A-Z][a-zA-Z]{2,})$";
     private final static String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    private final static String MOBILE_NUMBER = "^(\\d{2})( )([6-9]{1})(\\d{9})";
 
     public boolean validateName(String name){
         Pattern pattern = Pattern.compile(FIRST_NAME);
@@ -15,4 +16,10 @@ public class UserDetails {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         return pattern.matcher(email).matches();
     }
+
+    public boolean validateMobileNumber(String mobileNumber){
+        Pattern pattern = Pattern.compile(MOBILE_NUMBER);
+        return pattern.matcher(mobileNumber).matches();
+    }
+
 }
