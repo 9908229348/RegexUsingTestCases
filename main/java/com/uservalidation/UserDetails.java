840f1f3d7052291c7766cdfec3f2413f66e00a81
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-27 12:02:00 +0530

    Added: Program to Validate Password with min 8 Characters

--- main/java/com/uservalidation/UserDetails.java
+++ main/java/com/uservalidation/UserDetails.java
@@ -6,7 +6,7 @@ public class UserDetails {
     private final static String FIRST_NAME = "([A-Z][a-zA-Z]{2,})$";
     private final static String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
     private final static String MOBILE_NUMBER = "^(\\d{2})( )([6-9]{1})(\\d{9})";
-
+    private final static String PASSWORD = "^([a-zA-Z0-9!@#$%^&*.]{8,})$";
     public boolean validateName(String name){
         Pattern pattern = Pattern.compile(FIRST_NAME);
         return pattern.matcher(name).matches();
@@ -20,6 +20,11 @@ public class UserDetails {
     public boolean validateMobileNumber(String mobileNumber){
         Pattern pattern = Pattern.compile(MOBILE_NUMBER);
         return pattern.matcher(mobileNumber).matches();
+    }
+
+    public boolean validatePassWord(String passWord){
+        Pattern pattern = Pattern.compile(PASSWORD);
+        return pattern.matcher(passWord).matches();
     }
 
 }
