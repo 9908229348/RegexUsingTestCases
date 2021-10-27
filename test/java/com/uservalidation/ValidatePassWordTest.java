cc16ea1a418faed025c72ddda8464090ab26c728
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-27 13:37:43 +0530

    Added: Test Program for password validatiom Rule3

--- test/java/com/uservalidation/ValidatePassWordTest.java
+++ test/java/com/uservalidation/ValidatePassWordTest.java
@@ -21,9 +21,17 @@ public class ValidatePassWordTest {
     }
 
     @Test
-    void givenOneCapital_CheckForValidation_ReturnTrue() {
+    void givenOneCapital_CheckForValidation_ReturnFalse() {
+        UserDetails userDetails = new UserDetails();
+        boolean result = userDetails.validatePassWord("Sravanthi");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void givenOneNumeric_CheckForValidation_ReturnTrue() {
         UserDetails userDetails = new UserDetails();
         boolean result = userDetails.validatePassWord("Sravanthi@446");
         Assertions.assertTrue(result);
     }
 }
+
