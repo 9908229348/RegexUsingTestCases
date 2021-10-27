9bfb632161d0357fa1d2c37dafd52d911af20e2c
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-27 14:13:32 +0530

    Added: Email Samples Validation

--- main/java/com/uservalidation/UserDetails.java
+++ main/java/com/uservalidation/UserDetails.java
@@ -7,6 +7,7 @@ public class UserDetails {
     private final static String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
     private final static String MOBILE_NUMBER = "^(\\d{2})( )([6-9]{1})(\\d{9})";
     private final static String PASSWORD = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]{1})(?=\\S+$).{8,}";
+
     public boolean validateName(String name){
         Pattern pattern = Pattern.compile(FIRST_NAME);
         return pattern.matcher(name).matches();
