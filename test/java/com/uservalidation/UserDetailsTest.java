fdcda2725da24f19157f34a618209e67d7137dc8
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-28 12:08:16 +0530

    Added: Test Program for User Validation

--- test/java/com/uservalidation/UserDetailsTest.java
+++ test/java/com/uservalidation/UserDetailsTest.java
@@ -1,69 +1,20 @@
 package com.uservalidation;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class UserDetailsTest {
     @Test
-    void givenFirstName_CheckValidationForFName_ReturnTrue() {
+    public void givenDetails_CheckForValidation_ReturnHappy(){
         UserDetails userDetails = new UserDetails();
-        boolean result = userDetails.validateName("Savant");
-        Assertions.assertTrue(result);
+        String result = userDetails.validateDetails("Kallavai","Sravanthi","kalavaisravanthi99@gmail.com","98 9908229348","Sravanthi@446");
+        Assert.assertEquals("Happy",result);
     }
 
     @Test
-    void givenFirstLetterSmall_CheckForValidationForFName_ReturnFalse() {
+    public void givenNonMatchingDetails_CheckForValidation_ReturnSad() {
         UserDetails userDetails = new UserDetails();
-        boolean result = userDetails.validateName("savant");
-        Assertions.assertFalse(result);
-    }
-
-    @Test
-    void givenTwoLetters_CheckForValidationForLName_ReturnFalse() {
-        UserDetails userDetails = new UserDetails();
-        boolean result = userDetails.validateName("Sr");
-        Assertions.assertFalse(result);
-    }
-
-    @Test
-    void givenTwoSmalls_CheckForValidationForLName_RerunFalse() {
-        UserDetails userDetails = new UserDetails();
-        boolean result = userDetails.validateName("sr");
-        Assertions.assertFalse(result);
-    }
-
-    @Test
-    void givenEmail_CheckValidation_ReturnTrue() {
-        UserDetails userDetails = new UserDetails();
-        boolean result = userDetails.validateEmail("abc.xyz@gmail.com");
-        Assertions.assertTrue(result);
-    }
-
-    @Test
-    void givenEmailWithoutTld_CheckValidation_ReturnFalse() {
-        UserDetails userDetails = new UserDetails();
-        boolean result = userDetails.validateEmail("abc.xyz@gmail");
-        Assertions.assertFalse(result);
-    }
-
-    @Test
-    void givenMobileNumber_CheckForValidation_ReturnTrue() {
-        UserDetails userDetails = new UserDetails();
-        boolean result = userDetails.validateMobileNumber("98 9908229348");
-        Assertions.assertTrue(result);
-    }
-
-    @Test
-    void givenNumberWithoutCC_CheckForValidation_ReturnFalse() {
-        UserDetails userDetails = new UserDetails();
-        boolean result = userDetails.validateMobileNumber("9908229348");
-        Assertions.assertFalse(result);
-    }
-
-    @Test
-    void givenShortNumber_CheckForValidation_ReturnFalse() {
-        UserDetails userDetails = new UserDetails();
-        boolean result = userDetails.validateMobileNumber("98 990822");
-        Assertions.assertFalse(result);
+        String result = userDetails.validateDetails("kallavai","Sravanthi","kalavaisravanthi99@gmail.com","98 9908229348","Sravanthi@446");
+        Assert.assertEquals("Sad",result);
     }
 }
